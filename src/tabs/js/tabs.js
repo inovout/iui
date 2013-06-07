f02d49f3ea823e495b2731ef06b3c8416b510550
Ristona <ristona@163.com>
2013-06-07 21:34:53 +0800

    完成tabs样式

--- src/tabs/js/tabs.js
+++ src/tabs/js/tabs.js
@@ -1,4 +1,14 @@
 var Tabs = Class.create({
-    initialize: function () {
+    initialize: function (element) {
+        if (typeof element == "string") {
+            this.viewObject = $("#" + element);
+        } else {
+            this.viewObject = $(element);
+        }
+        this.selectedChanged = new Event("selectedChanged", this);
+
+        this.viewObject.bind('click', function() {
+            alert($(this).text());
+        });
     }
 });
--- src/tabs/tests/tabs-tests.js
+++ src/tabs/tests/tabs-tests.js
@@ -1,7 +1,9 @@
+
+
+
 test("Tabs Tests", function () {
-    var car = new Car();
-    ok(!Object.isUndefined(car.runned), "event object is not null!");
-    ok(car.runned.name == "runned", "event name!");
-    ok(car.runned.observable == car, "event observable!");
+    debugger;
+    var tabs = new Tabs("testTabs");
+    ok(!Object.isUndefined(tabs), "tabs is not null!");
 });
 

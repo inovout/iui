117944019e119d81be4ba1cb2bf7c049824b625a
qianwei <moneywei_2008@163.com>
2013-07-01 13:13:14 +0800

    将 form 的样式 添加到 cwidget 和 twidget

--- build/build.bat
+++ build/build.bat
@@ -2,8 +2,8 @@
 
 echo css文件开始复制
 copy ..\src\cssreset\css\cssreset.css+..\src\cssfonts\css\cssfonts.css+..\src\csslayout\css\csslayout.css cSun.css /b
-copy ..\src\cssbase\css\cssbase.css+..\src\list\css\list.css+..\src\searcher\css\searcher.css+..\src\nav\css\nav.css+..\src\table\css\table.css+..\src\article\css\article.css+..\src\chart\css\chart.css cWidget.css /b
-copy ..\src\list\css\list.theme.css+..\src\searcher\css\searcher.theme.css+..\src\nav\css\nav.theme.css+..\src\table\css\table.theme.css+..\src\article\css\article.theme.css+..\src\chart\css\chart.theme.css tWidget.css /b
+copy ..\src\cssbase\css\cssbase.css+..\src\list\css\list.css+..\src\searcher\css\searcher.css+..\src\nav\css\nav.css+..\src\table\css\table.css+..\src\article\css\article.css+..\src\chart\css\chart.css+..\src\form\css\form.css cWidget.css /b
+copy ..\src\list\css\list.theme.css+..\src\searcher\css\searcher.theme.css+..\src\nav\css\nav.theme.css+..\src\table\css\table.theme.css+..\src\article\css\article.theme.css+..\src\chart\css\chart.theme.css+..\src\form\css\form.theme.css tWidget.css /b
 copy ..\src\cssfonts\css\cssfonts.site.css+..\src\sitelogo\css\sitelogo.css cSite.css /b
 echo css文件复制完成
 

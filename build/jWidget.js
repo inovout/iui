8bef41356a7867137c961cffba8e4332d6162d17
qianwei <moneywei_2008@163.com>
2013-07-02 11:03:10 +0800

    修改笔误 40em 改为78em

--- build/jWidget.js
+++ build/jWidget.js
@@ -41,8 +41,9 @@ Inovout.Widget.TabList = Class.create(Inovout.View, {
 Inovout.Widget.DataChart = Class.create(Inovout.View, {
     initialize: function ($super, element) {
         $super(element);
-        debugger;
-        $(element.dom).highcharts(element.html().evalJSON());
+        var option = new Function("return " + element.html().replace(/\n/g,""))();
+        $(element.dom).highcharts(option);
+        //$(element.dom).highcharts(eval("(" + element.html() + ")"));
         element.css("visibility", "visible");
     }
 });

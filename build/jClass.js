1ee8d3d53b2b63acc6460cc02efa4ee68dab0630
qianwei <moneywei_2008@163.com>
2013-07-03 13:51:13 +0800

    改变csslayout 文件的编码

--- build/jClass.js
+++ build/jClass.js
@@ -5807,3 +5807,17 @@ Object.extend(Inovout.View, {
 })(this, this.document);
 
 
+Inovout.Widget.DataChart = Class.create(Inovout.View, {
+    initialize: function ($super, element) {
+        $super(element);
+        var option = new Function("return " + element.html().replace(/\n/g,""))();
+        $(element.dom).highcharts(option);
+        //$(element.dom).highcharts(eval("(" + element.html() + ")"));
+        element.css("visibility", "visible");
+    }
+});
+$(function () {
+    $(".dataChart").each(function (i, dom) {
+        Inovout.View.get(dom);
+    });
+});

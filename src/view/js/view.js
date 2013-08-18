a5dd8ecdf7651933c5be566c0e5b6300689a9f1e
Ristona <ristona@163.com>
2013-08-18 14:41:56 +0800

    修改变量名错误

--- src/view/js/view.js
+++ src/view/js/view.js
@@ -17,25 +17,25 @@ Object.extend(Inovout.View, {
         element = Inovout.Element.get(element);
         var view = Inovout.View.cache.get(element);
         if (!view) {
-            var vidgetClass;
+            var widgetType;
             for (var viewClass in Inovout.View.factories) {
                 if (Inovout.View.factories[viewClass](element)) {
-                    vidgetClass = viewClass;
+                    widgetType = viewClass;
                     break
                 }
             }
-            if (!vidgetClass) {
+            if (!widgetType) {
                 for (var widget in Inovout.Widget) {
                     var wc = widget.substring(0, 1).toLowerCase() + widget.substring(1, widget.length);
                     if (element.hasClass(wc)) {
-                        vidgetClass = widget;
+                        widgetType = widget;
                         Inovout.View.factories[widget] = function (ele) { return ele.hasClass(wc); };
                         break
                     }
                 }
             }
-            if (vidgetClass) {
-                view = new Inovout.Widget[vidgetClass](element);
+            if (widgetType) {
+                view = new Inovout.Widget[widgetType](element);
             } else {
                 view = new Inovout.View(element);
                 Object.extend(view, element);
